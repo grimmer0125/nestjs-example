a775404068cf30bef6b41c044d49a52d1cccdedf
Grimmer <grimmer0125@gmail.com>
2020-07-15 12:42:36 +0800

    Use request-scoped providers to implement request-scoped strategies
    JwtAuthGuard is customized and defaultStrategy is applied, too.

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,16 +1,33 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { ContextIdFactory } from '@nestjs/core/helpers/context-id-factory';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
-    super();
+  // constructor(private authService: AuthService) {
+  //   super();
+  // }
+
+  constructor(private authService: AuthService, private moduleRef: ModuleRef) {
+    super({
+      passReqToCallback: true,
+    });
   }
 
-  async validate(username: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(username, password);
+  async validate(
+    request: Request,
+    username: string,
+    password: string,
+  ): Promise<any> {
+    const contextId = ContextIdFactory.getByRequest(request);
+
+    // "AuthService" is a request-scoped provider
+    const authService = await this.moduleRef.resolve(AuthService, contextId);
+
+    const user = await authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
     }
