8dbb6ffa4737ac6dc7d3b2038d99445d7dbf97d3
Grimmer <grimmer0125@gmail.com>
2020-07-15 11:47:03 +0800

    Add passport-local auth example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AuthorsModule } from './authors/authors.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { AuthorsModule } from './authors/authors.module';
     GraphQLModule.forRoot({
       autoSchemaFile: true, //process.cwd() + 'src/schema.gql',
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
